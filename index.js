3de4d418fdcb79d6fbfdceee47425852a34eba51
Worlanyo <worladamalie@gmail.com>
2023-07-29 17:27:28 +0000

    Fix issues
    Resolve cors issue
    Fix undefined issue with  .GOOGLE_SHEETS_PRIVATE_KEY env variable

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
+require('dotenv').config();
 const express = require('express');
 const { google } = require("googleapis");
 const port = process.env.PORT || 3001
 const app = express();
 const cors = require('cors');
+const options = {
+    origin: ['http://localhost:3000','https://worlanyodamalie.github.io/dynamo',  'https://exploredynamo.com' , 'https://dynamo-tau.vercel.app','https://1gallon.com.gh'],
+    methods: 'POST',
+}
+
+
+app.use(cors(options))
+
 app.use(express.json());
 
+const authSheet = async () => {
+    const jwt = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+      },
+      scopes: [
+        "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/spreadsheets",
+      ],
+    });
+    const sheets = google.sheets({ version: "v4", auth: jwt });
 
-app.listen(port, () => {
-    console.log(`Server is running on ${port}`)
-})
+    return { sheets }
 
-app.post('/sheet' , cors() , async (req,res) => {
+}
+
+
+
+app.post('/sheet' ,  async (req,res) => {
     const contact = req.body
 
-    console.log(`this is  ${contact}`)
 
     try {
 
-        const jwt = new google.auth.GoogleAuth(
-          {  credentials: {
-                client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-                private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        // const jwt = new google.auth.GoogleAuth(
+        //   {  credentials: {
+        //         client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+        //         private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n"),
 
-            },
-            scopes: [
-                'https://www.googleapis.com/auth/drive',
-                'https://www.googleapis.com/auth/drive.file',
-                'https://www.googleapis.com/auth/spreadsheets'
-            ]
-        }
-          );
-          const sheets = google.sheets({ version: "v4", auth: jwt });
+        //     },
+        //     scopes: [
+        //         'https://www.googleapis.com/auth/drive',
+        //         'https://www.googleapis.com/auth/drive.file',
+        //         'https://www.googleapis.com/auth/spreadsheets'
+        //     ]
+        // }
+        //   );
+        //   const sheets = google.sheets({ version: "v4", auth: jwt });
+        const { sheets } = await authSheet();
 
           const response = await sheets.spreadsheets.values.append({
             spreadsheetId: process.env.SPREADSHEET_ID,
@@ -41,7 +65,7 @@ app.post('/sheet' , cors() , async (req,res) => {
             },
           });
 
-        //   console.log("response",response)
+          console.log("response",response)
           return res.status(201).json({
              data: response.data,
              status: response.status
@@ -49,8 +73,13 @@ app.post('/sheet' , cors() , async (req,res) => {
 
     } catch (e) {
         // console.log("error",e)
-        return res.status(e.code).send({message: e.message})
+        return res.status(e.code || 500 ).send({message: e.message})
     }
 
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
 })
 
