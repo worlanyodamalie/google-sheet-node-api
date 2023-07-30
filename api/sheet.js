94142b8c3ef928b8e62576f10dab503b5eddd446
Worlanyo <worladamalie@gmail.com>
2023-07-30 08:33:51 +0000

    Refactor function and set response headers manually

--- api/sheet.js
+++ api/sheet.js
@@ -1,60 +1,61 @@
 const { google } = require("googleapis");
-const cors = require("micro-cors")();
-
-const authSheet = async () => {
-    const jwt = new google.auth.GoogleAuth({
-      credentials: {
-        client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
-      },
-      scopes: [
-        "https://www.googleapis.com/auth/drive",
-        "https://www.googleapis.com/auth/drive.file",
-        "https://www.googleapis.com/auth/spreadsheets",
-      ],
-    });
-    const sheets = google.sheets({ version: "v4", auth: jwt });
-
-    return { sheets }
-
-}
-
-const corsOptions = {
-    allowMethods: ['POST'],
-    origin: [ 'https://exploredynamo.com' ,'https://1gallon.com.gh'], // Replace with your actual domain
-};
-
-const corsHandler = cors(corsOptions);
 
 
+module.exports = async (req,res) => {
+    const origin = req.headers.origin;
 
-const sheetHandler = async (req,res) => {
-    const contact = req.body
+    const allowedOrigin = [ 'https://exploredynamo.com' ,'https://1gallon.com.gh','https://exploredynamo.com/','https://1gallon.com.gh/'];
 
-    try {
-     
-        const spreadsheetId = contact?.type === "dynamo" ? process.env.SPREADSHEET_ID : process.env.ONE_GALLON_SPREADSHEET_ID
-        const { sheets } = await authSheet();
+    if(allowedOrigin.includes(origin)){
+        res.setHeader("Access-Control-Allow-Origin", origin);
+        res.setHeader("Access-Control-Allow-Methods", "POST");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-        //console.log(`spreadsheetId: ${spreadsheetId}`)
-        const response = await sheets.spreadsheets.values.append({
-            spreadsheetId:  spreadsheetId,
-            range: contact?.range ,
-            valueInputOption: 'USER_ENTERED',
-            requestBody: {
-              values: [contact?.data],
-            },
-          });
+        if(req.method === "POST"){
+            const contact = req.body
+            try {
+                const spreadsheetId =
+                contact?.type === "dynamo"
+                    ? process.env.SPREADSHEET_ID
+                    : process.env.ONE_GALLON_SPREADSHEET_ID;
 
-          return res.status(201).json({
-             data: response.data,
-             status: response.status
-          })
+                const jwt = new google.auth.GoogleAuth({
+                        credentials: {
+                          client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+                          private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+                        },
+                        scopes: [
+                          "https://www.googleapis.com/auth/drive",
+                          "https://www.googleapis.com/auth/drive.file",
+                          "https://www.googleapis.com/auth/spreadsheets",
+                        ],
+                      });
+                
+                    const sheets = google.sheets({ version: "v4", auth: jwt }); 
+                    
+                    const response = await sheets.spreadsheets.values.append({
+                        spreadsheetId: spreadsheetId,
+                        range: contact?.range,
+                        valueInputOption: "USER_ENTERED",
+                        requestBody: {
+                            values: [contact?.data],
+                        },
+                    });
 
-    } catch (e) {
-        // console.log("error",e)
-        return res.status(e.code || 500 ).send({message: e.message})
+                    return res.status(201).json({
+                        data: response.data,
+                        status: response.status,
+                    });
+
+
+            } catch (e) {
+                return res.status(e.code || 500).send({ message: e.message });
+            }
+        }else{
+            return res.status(405).send({ message: "Method Not Allowed" });
+        }
+
+    }else{
+        return res.status(403).send({ message: "Forbidden" });
     }
 }
-
-module.exports = corsHandler(sheetHandler);
