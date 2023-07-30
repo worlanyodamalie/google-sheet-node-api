d9e6655e78e7c438030b2ba4a84a955e43662947
Worlanyo <worladamalie@gmail.com>
2023-07-30 06:10:43 +0000

    convert to serverless function for vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,94 +1,84 @@
-require('dotenv').config();
-const express = require('express');
-const { google } = require("googleapis");
-const port = process.env.PORT || 3001
-const app = express();
-const cors = require('cors');
-const options = {
-    //uncomment when pushing
-    origin: [ 'https://exploredynamo.com' ,'https://1gallon.com.gh'],
-   //comment when deploying
-    // origin: "http://localhost:3000/",
-    methods: 'POST',
-}
+// require('dotenv').config();
+// const express = require('express');
+// const { google } = require("googleapis");
+// const port = process.env.PORT || 3001
+// const app = express();
+// const cors = require('cors');
+// const options = {
+//     //uncomment when pushing
+//     origin: [ 'https://exploredynamo.com' ,'https://1gallon.com.gh'],
+//    //comment when deploying
+//     // origin: "http://localhost:3000/",
+//     methods: 'POST',
+// }
 
 
-app.use(cors(options))
+// app.use(cors(options))
 
-app.use(express.json());
+// app.use(express.json());
 
-const authSheet = async () => {
-    const jwt = new google.auth.GoogleAuth({
-      credentials: {
-        client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
-      },
-      scopes: [
-        "https://www.googleapis.com/auth/drive",
-        "https://www.googleapis.com/auth/drive.file",
-        "https://www.googleapis.com/auth/spreadsheets",
-      ],
-    });
-    const sheets = google.sheets({ version: "v4", auth: jwt });
+// const authSheet = async () => {
+//     const jwt = new google.auth.GoogleAuth({
+//       credentials: {
+//         client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+//         private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+//       },
+//       scopes: [
+//         "https://www.googleapis.com/auth/drive",
+//         "https://www.googleapis.com/auth/drive.file",
+//         "https://www.googleapis.com/auth/spreadsheets",
+//       ],
+//     });
+//     const sheets = google.sheets({ version: "v4", auth: jwt });
 
-    return { sheets }
+//     return { sheets }
 
-}
+// }
 
-app.get('/api', (req, res) => {
-    res.send('This is a Google sheet node api..')
-  })
+// app.get('/api', (req, res) => {
+//     res.send('This is a Google sheet node api..')
+//   })
 
-app.post('/api/sheet' ,  async (req,res) => {
-    const contact = req.body
+// app.post('/api/sheet' ,  async (req,res) => {
+//     const contact = req.body
 
 
-    try {
+//     try {
 
-        // const jwt = new google.auth.GoogleAuth(
-        //   {  credentials: {
-        //         client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-        //         private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      
+//         const spreadsheetId = contact?.type === "dynamo" ? process.env.SPREADSHEET_ID : process.env.ONE_GALLON_SPREADSHEET_ID
+//         const { sheets } = await authSheet();
 
-        //     },
-        //     scopes: [
-        //         'https://www.googleapis.com/auth/drive',
-        //         'https://www.googleapis.com/auth/drive.file',
-        //         'https://www.googleapis.com/auth/spreadsheets'
-        //     ]
-        // }
-        //   );
-        //   const sheets = google.sheets({ version: "v4", auth: jwt });
-        const spreadsheetId = contact?.type === "dynamo" ? process.env.SPREADSHEET_ID : process.env.ONE_GALLON_SPREADSHEET_ID
-        const { sheets } = await authSheet();
-
-        //console.log(`spreadsheetId: ${spreadsheetId}`)
+//         //console.log(`spreadsheetId: ${spreadsheetId}`)
 
        
 
-        const response = await sheets.spreadsheets.values.append({
-            spreadsheetId:  spreadsheetId,
-            range: contact?.range ,
-            valueInputOption: 'USER_ENTERED',
-            requestBody: {
-              values: [contact?.data],
-            },
-          });
+//         const response = await sheets.spreadsheets.values.append({
+//             spreadsheetId:  spreadsheetId,
+//             range: contact?.range ,
+//             valueInputOption: 'USER_ENTERED',
+//             requestBody: {
+//               values: [contact?.data],
+//             },
+//           });
 
-          return res.status(201).json({
-             data: response.data,
-             status: response.status
-          })
+//           return res.status(201).json({
+//              data: response.data,
+//              status: response.status
+//           })
 
-    } catch (e) {
-        // console.log("error",e)
-        return res.status(e.code || 500 ).send({message: e.message})
-    }
+//     } catch (e) {
+//         // console.log("error",e)
+//         return res.status(e.code || 500 ).send({message: e.message})
+//     }
 
-})
+// })
 
 
-app.listen(port, () => {
-    console.log(`Server is running on ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Server is running on ${port}`)
+// })
 
+module.exports = (req, res) => {
+  res.status(200).json({ message: 'Hello from Vercel serverless function!' });
+};
---
+++ api/sheet.js
@@ -0,0 +1,61 @@
+const { google } = require("googleapis");
+const cors = require("micro-cors")();
+
+const authSheet = async () => {
+    const jwt = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+      },
+      scopes: [
+        "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/spreadsheets",
+      ],
+    });
+    const sheets = google.sheets({ version: "v4", auth: jwt });
+
+    return { sheets }
+
+}
+
+const corsOptions = {
+    allowMethods: ['POST'],
+    origin: [ 'https://exploredynamo.com' ,'https://1gallon.com.gh'], // Replace with your actual domain
+};
+
+const corsHandler = cors(corsOptions);
+
+module.exports = corsHandler(async (req,res) => {
+    const contact = req.body
+
+
+    try {
+
+      
+        const spreadsheetId = contact?.type === "dynamo" ? process.env.SPREADSHEET_ID : process.env.ONE_GALLON_SPREADSHEET_ID
+        const { sheets } = await authSheet();
+
+        //console.log(`spreadsheetId: ${spreadsheetId}`)
+
+       
+
+        const response = await sheets.spreadsheets.values.append({
+            spreadsheetId:  spreadsheetId,
+            range: contact?.range ,
+            valueInputOption: 'USER_ENTERED',
+            requestBody: {
+              values: [contact?.data],
+            },
+          });
+
+          return res.status(201).json({
+             data: response.data,
+             status: response.status
+          })
+
+    } catch (e) {
+        // console.log("error",e)
+        return res.status(e.code || 500 ).send({message: e.message})
+    }
+})
