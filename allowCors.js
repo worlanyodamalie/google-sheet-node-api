80d87dd58b8ce3dadae9911ebe2cd2f296c08650
Worlanyo <worladamalie@gmail.com>
2023-08-01 11:10:35 +0000

    Fix cors issues and refactoring of code

--- allowCors.js
+++ allowCors.js
@@ -1,23 +1,44 @@
-const allowCors = (allowedOrigins) => (fn) => async (req, res) => {
-    const origin = req.headers.origin;
-  
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Credentials', true);
-      res.setHeader('Access-Control-Allow-Origin', origin);
-      // Set other CORS headers as needed
-      res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  
-      if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-      }
-  
-      return await fn(req, res);
-    } else {
-      return res.status(403).send({ message: 'Forbidden' });
+const allowedOrigins = [ 
+  'https://exploredynamo.com' ,
+  'https://1gallon.com.gh',
+  'https://exploredynamo.com/',
+  'https://1gallon.com.gh/',
+];
+
+const allowCors = fn => async (req, res) => {
+
+  const origin = req.headers.origin
+
+  if(allowedOrigins.includes(origin)){
+    res.setHeader('Access-Control-Allow-Credentials', true)
+
+    res.setHeader('Access-Control-Allow-Origin', origin)
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+
+   res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+   )
+
+   if (req.method === 'OPTIONS') {
+    //res.setHeader('Access-Control-Allow-Origin', origin)
+    res.status(200).end()
+
+    return
+
     }
-  };
-  
-  module.exports = allowCors;
-  
+    return await fn(req, res)
+  }else {
+    return res.status(403).send({ message: 'Forbidden' });
+  }
+
+
+
+}
+
+
+
+export default allowCors
+
+
--- api/sheet.js
+++ api/sheet.js
@@ -1,20 +1,12 @@
 const { google } = require("googleapis");
-const allowCors = require('../allowCors');
+// const allowCors = require('../allowCors');
+import allowCors from '../allowCors'
 
-const allowedOrigins = [ 'https://exploredynamo.com' ,'https://1gallon.com.gh','https://exploredynamo.com/','https://1gallon.com.gh/'];
+const allowedOrigins = [ 'https://exploredynamo.com' ,'https://1gallon.com.gh','https://exploredynamo.com/','https://1gallon.com.gh/','http://localhost:3000'];
 
-const processSheet = async (req,res) => {
+ export default  async function handler(req,res){
     const contact = req.body;
     const origin = req.headers.origin;
-
-    if (req.method === 'OPTIONS') {
-        // Respond with a 200 status code for preflight requests
-        return res.status(200).end();
-      }
-
-      if (!allowedOrigins.includes(origin)) {
-        return res.status(403).send({ message: 'Forbidden' });
-      }
 
       if(req.method === 'POST'){
              try {
@@ -60,69 +52,6 @@ const processSheet = async (req,res) => {
   
 }
 
-module.exports = allowCors(allowedOrigins)(processSheet);
+// module.exports = allowCors(allowedOrigins)(handler);
+module.exports = allowCors(handler)
 
-// module.exports = async (req,res) => {
-//     const origin = req.headers.origin;
-
-//     const allowedOrigin = [ 'https://exploredynamo.com' ,'https://1gallon.com.gh','https://exploredynamo.com/','https://1gallon.com.gh/'];
-
-//     if(allowedOrigin.includes(origin)){
-//         res.setHeader("Access-Control-Allow-Origin", origin);
-//         res.setHeader("Access-Control-Allow-Methods", "POST");
-//         res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-//          // Handle preflight requests
-//          if (req.method === "OPTIONS") {
-//             // Respond with a 200 status code for preflight requests
-//             return res.status(200).end();
-//         }
-
-//         if(req.method === "POST"){
-//             const contact = req.body
-//             try {
-//                 const spreadsheetId =
-//                 contact?.type === "dynamo"
-//                     ? process.env.SPREADSHEET_ID
-//                     : process.env.ONE_GALLON_SPREADSHEET_ID;
-
-//                 const jwt = new google.auth.GoogleAuth({
-//                         credentials: {
-//                           client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-//                           private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
-//                         },
-//                         scopes: [
-//                           "https://www.googleapis.com/auth/drive",
-//                           "https://www.googleapis.com/auth/drive.file",
-//                           "https://www.googleapis.com/auth/spreadsheets",
-//                         ],
-//                       });
-                
-//                     const sheets = google.sheets({ version: "v4", auth: jwt }); 
-                    
-//                     const response = await sheets.spreadsheets.values.append({
-//                         spreadsheetId: spreadsheetId,
-//                         range: contact?.range,
-//                         valueInputOption: "USER_ENTERED",
-//                         requestBody: {
-//                             values: [contact?.data],
-//                         },
-//                     });
-
-//                     return res.status(201).json({
-//                         data: response.data,
-//                         status: response.status,
-//                     });
-
-
-//             } catch (e) {
-//                 return res.status(e.code || 500).send({ message: e.message });
-//             }
-//         }else{
-//             return res.status(405).send({ message: "Method Not Allowed" });
-//         }
-
-//     }else{
-//         return res.status(403).send({ message: "Forbidden" });
-//     }
-// }
