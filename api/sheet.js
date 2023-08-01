1f4f9bf9ea7cace149b92546ee51891a3e6bac42
Worlanyo <worladamalie@gmail.com>
2023-08-01 12:31:59 +0000

    Fix to "error:1E08010C:DECODER routines::unsupported"

--- api/sheet.js
+++ api/sheet.js
@@ -18,7 +18,7 @@ const allowedOrigins = [ 'https://exploredynamo.com' ,'https://1gallon.com.gh','
                 const jwt = new google.auth.GoogleAuth({
                   credentials: {
                     client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-                    private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+                    private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n"),
                   },
                   scopes: [
                     "https://www.googleapis.com/auth/drive",
